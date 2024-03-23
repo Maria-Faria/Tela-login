7bf2548ba9ac7f2edccb722326f9517bda293348
Maria Eduarda <mariaeduardadefaria15@gmail.com>
2024-03-23 20:11:38 -0300

    tela-login

---
+++ backend/src/routes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const routes = express.Router();
+const users = [{
+    id: 1,
+    name: 'John Doe',
+    email: 'john@example.com',
+    password: '123456'
+}]
+
+routes.post('/login', (req, res) => {
+    const {email, password} = req.body;
+
+    const user = users.find(user => user.email === email && user.password === password);
+    
+    if(user) {
+        return res.status(200).json(user);
+    }
+
+    return res.status(401).json({error: 'Invalid credentials'});
+})
+
+module.exports = routes;
